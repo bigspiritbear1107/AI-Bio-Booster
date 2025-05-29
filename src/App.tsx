4fdecc39815b07f1b7f8b4cb0be186fd4d467c73
midas3110 <karmansolen3110@gmail.com>
2025-05-29 01:07:34 -0400

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import SignIn from './pages/Auth/Sign_in'
-import SignUp from './pages/Auth/Sign_up'
-import FreeVersion from './pages/FreeVersion'
-import PricingPage from './pages/PricingPage'
-import PaidvVersion from './pages/PaidVersion'
+import { Routes, Route, Navigate } from "react-router-dom";
+import SignIn from "./pages/Auth/Sign_in";
+import SignUp from "./pages/Auth/Sign_up";
+import FreeVersion from "./pages/FreeVersion";
+import PricingPage from "./pages/PricingPage";
+import PaidvVersion from "./pages/PaidVersion";
 
-import PremiumCheckout from './pages/PremiumCheckout'
+import PremiumCheckout from "./pages/PremiumCheckout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/signin" replace />} />
+      <Route path="/" element={<Navigate to="/free_version" replace />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/free_version" element={<FreeVersion />} />
       <Route path="/pricing" element={<PricingPage />} />
-      <Route path='/paid_version' element={<PaidvVersion />} />
-      <Route path='/premium_checkout' element={<PremiumCheckout />} />
+      <Route path="/paid_version" element={<PaidvVersion />} />
+      <Route path="/premium_checkout" element={<PremiumCheckout />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,15 @@ export default function Header() {
   return (
     <div className="fixed top-4 left-0 right-0 z-50 flex justify-center">
       <div className="w-full max-w-5xl px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center px-6 py-4 
-                    bg-gray-200/50 backdrop-blur-lg rounded-[15px] shadow-md">
+        <div
+          className="flex justify-between items-center px-6 py-4 
+                    bg-gray-200/50 backdrop-blur-lg rounded-[15px] shadow-md"
+        >
           {/* Logo and Brand Name */}
           <div className="flex items-center">
             <a href="#home" className="flex items-center">
               <img
-                src="../../public/images/logo.png"
+                src="/images/logo.png"
                 alt="AI Bio Booster"
                 className="h-10 w-10 rounded-full"
               />
@@ -26,17 +28,24 @@ export default function Header() {
           </div> */}
           {/* Section Navbar */}
           <div className="flex space-x-6 items-center text-sm font-medium">
-            <a href="#home" className="text-black hover:underline">Home</a>
-            <a href="#about" className="text-black hover:underline">About</a>
-            <a href="#faq" className="text-black hover:underline">FAQ</a>
-            <a href="#comments" className="text-black hover:underline">Comments</a>
-            <a href="#analysis" className="text-black hover:underline">Analysis</a>
+            <a href="#home" className="text-black hover:underline">
+              Home
+            </a>
+            <a href="#about" className="text-black hover:underline">
+              About
+            </a>
+            <a href="#faq" className="text-black hover:underline">
+              FAQ
+            </a>
+            <a href="#comments" className="text-black hover:underline">
+              Comments
+            </a>
+            <a href="#analysis" className="text-black hover:underline">
+              Analysis
+            </a>
           </div>
-
         </div>
       </div>
     </div>
-
-
   );
 }
